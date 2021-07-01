494469f2c155a7428b2d53b2ae0ca4c216c9a063
DendyAR <anugerah700@gmail.com>
2021-07-01 09:48:21 +0700

    initial commite

---
+++ function.js
@@ -0,0 +1,10 @@
+class OP {
+    constructor(data){
+        this.data = data
+    }
+
+    p(){
+        
+    }
+}
+module.exports=OP
---
+++ index.js
@@ -0,0 +1,9 @@
+const OP = require('./function')
+
+
+let input =0;
+let x = new OP(data)
+let Result = x.P(input)
+console.log(Result)
+
+
